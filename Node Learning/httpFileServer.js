31ef6236d025d09619df0c4f4418256781b133d2
kitler <stephen.r.schroeder@gmail.com>
2016-03-18 03:21:18 -0700

    Started fileserver challenge

---
+++ Node Learning/httpFileServer.js
@@ -0,0 +1,10 @@
+//Written by Stephen Schroeder
+//The following is a fileserver program built in node 
+var http = require('http');
+
+var fs = require('fs');
+
+var httpServer = http.createServer(function(request, response)
+{
+	
+})
