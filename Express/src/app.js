987614a6c07171f0185004085d8adfdb1b3bafde
kitler <stephen.r.schroeder@gmail.com>
2016-03-24 14:31:39 -0700

    Completed demo Express app

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,1 +1,37 @@
 'use strict'
+
+var express = require('express'),
+	posts = require('./mock/posts.json');
+
+var app = express();
+
+var postLists = Object.keys(posts).map(function(value){
+return posts[value];});
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.use('/static', express.static( __dirname +'/public'));
+
+app.get('/', function(req, res){
+	var path = req.path;
+	console.log("loaded");
+	res.locals.path = path;
+	res.render('index.jade');
+});
+
+app.get('/blog/:id?', function(req, res){
+	var title = req.params.id;
+	if(title === undefined){
+		res.status(503);
+		res.render('blog', {posts: postLists});
+	}else{
+		var rtnPosts = posts[title] || {};
+		res.render('posts.jade', {post: rtnPosts});
+	}
+	
+
+});
+app.listen(3000, function(){
+	console.log("The frontend server is running on Port 3000");
+});
